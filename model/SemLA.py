9f767296adb03cac21890092f12bf7db5d7083fe
RuichenNi <pengbo@gmail.com>
2024-01-19 16:28:30 +0800

    add some comment

--- model/SemLA.py
+++ model/SemLA.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import pdb
+import numpy as np 
+from PIL import Image
 from einops.einops import rearrange
 from .reg import SemLA_Reg
 from .fusion import SemLA_Fusion
@@ -18,26 +21,47 @@ class SemLA(nn.Module):
             thr = 0.5
         elif matchmode=='scene':
             thr = 0
+
+        """
+        feat_reg_vi_final, feat_reg_ir_final [1,256,60,80] N C H W
+        feat_sa_vi, feat_sa_ir [1,1,60,80] N C H W
+        """
         feat_reg_vi_final, feat_reg_ir_final, feat_sa_vi, feat_sa_ir = self.backbone(
             torch.cat((img_vi, img_ir), dim=0))
+        #pdb.set_trace()
 
 
+        """
+        sa_vi, sa_ir [4800] 60x80
+        feat_reg_vi,feat_reg_ir [1,4800,256] N HxW C
+        """
         sa_vi, sa_ir = feat_sa_vi.reshape(-1), feat_sa_ir.reshape(-1)
         sa_vi, sa_ir = torch.where(sa_vi > thr)[0], torch.where(sa_ir > thr)[0]
 
         feat_reg_vi = rearrange(feat_reg_vi_final, 'n c h w -> n (h w) c')
         feat_reg_ir = rearrange(feat_reg_ir_final, 'n c h w -> n (h w) c')
+        #pdb.set_trace()
 
+        """
+        feat_reg_vi, feat_reg_ir [1,4800,256] N HxW C
+        """
         feat_reg_vi, feat_reg_ir = feat_reg_vi[:, sa_vi], feat_reg_ir[:, sa_ir]
         feat_reg_vi, feat_reg_ir = map(lambda feat: feat / feat.shape[-1] ** .5,
                                        [feat_reg_vi, feat_reg_ir])
+        #pdb.set_trace()
 
+
+        """
+        conf [1,4800,4800]
+        mask [1,4800,4800]
+        """
         conf = torch.einsum("nlc,nsc->nls", feat_reg_vi,
                             feat_reg_ir) / 0.1
         mask = conf > 0.
         mask = mask \
                * (conf == conf.max(dim=2, keepdim=True)[0]) \
                * (conf == conf.max(dim=1, keepdim=True)[0])
+        pdb.set_trace()
 
         mask_v, all_j_ids = mask.max(dim=2)
         b_ids, i_ids = torch.where(mask_v)
@@ -51,8 +75,8 @@ class SemLA(nn.Module):
         mkpts1 = torch.stack(
             [j_ids % feat_sa_vi.shape[3], j_ids // feat_sa_vi.shape[3]],
             dim=1) * 8
-
+        pdb.set_trace()
         sa_ir= F.interpolate(feat_sa_ir, scale_factor=8, mode='bilinear', align_corners=True)
         sa_vi= F.interpolate(feat_sa_vi, scale_factor=8, mode='bilinear', align_corners=True)
-
+        pdb.set_trace()
         return mkpts0, mkpts1, feat_sa_vi, feat_sa_ir, sa_ir, sa_vi
